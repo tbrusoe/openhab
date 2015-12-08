39b3064b6cede8b88542e87d0ba3226a6b72f3a4
Tyler Brusoe <tbrusoe@terpmail.umd.edu>
2015-12-08 15:25:06 -0500

    remove hab

--- bundles/io/org.openhab.io.habmin/src/main/java/org/openhab/io/habmin/services/zwave/ActionBean.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.io.innovationhub.services.zwave;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name="action")
-public class ActionBean {
-	public String domain;
-	public String action;
-}
